e66f11e5ab24709374006a457b9a65ff34702f4b
Dieter Van Staey <58091947+DieterVanStaey@users.noreply.github.com>
2021-10-13 10:23:50 +0200

    Veilingen

---
+++ TDDTaken/Veiling.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDTaken
+{
+    public class Veiling
+    {
+        private decimal hoogsteBod;
+        public void DoeBod(decimal bedrag)
+        {
+            if (bedrag >= HoogsteBod)
+                hoogsteBod = bedrag;
+        }
+
+        public decimal HoogsteBod
+        {
+            get
+            {
+                return hoogsteBod;
+            }
+        }
+    }
+}
---
+++ TDDTakenTest/VeilingTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDTaken;
+
+namespace TDDTakenTest
+{
+    [TestClass]
+    public class VeilingTest
+    {
+        [TestMethod]
+        public void HoogsteBodIsNulIndienErGeenBodGedaanIs()
+        {
+            var veiling = new Veiling();
+            Assert.AreEqual(decimal.Zero, veiling.HoogsteBod);
+        }
+
+        [TestMethod]
+        public void HoogsteBodIsEersteBedragNaEersteBod()
+        {
+            var veiling = new Veiling();
+            veiling.DoeBod(2000m);
+            Assert.AreEqual(2000m, veiling.HoogsteBod);
+        }
+
+        [TestMethod]
+        public void HoogsteBodAlsMeerdereBiedingenZijnGedaanIsHoogsteBod()
+        {
+            var veiling = new Veiling();
+            veiling.DoeBod(1000m);
+            veiling.DoeBod(2000m);
+            veiling.DoeBod(1500m);
+            Assert.AreEqual(2000m, veiling.HoogsteBod);
+        }
+    }
+}
