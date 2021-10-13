ea43bc3474417bb2ed24207c3e4a3df150178721
Dieter Van Staey <58091947+DieterVanStaey@users.noreply.github.com>
2021-10-13 12:29:21 +0200

    TestInitialize

--- TDDTakenTest/VeilingTest.cs
+++ TDDTakenTest/VeilingTest.cs
@@ -6,17 +6,24 @@ namespace TDDTakenTest
     [TestClass]
     public class VeilingTest
     {
+        private Veiling veiling;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            veiling = new Veiling();
+        }
         [TestMethod]
         public void HoogsteBodIsNulIndienErGeenBodGedaanIs()
         {
-            var veiling = new Veiling();
+            //var veiling = new Veiling();
             Assert.AreEqual(decimal.Zero, veiling.HoogsteBod);
         }
 
         [TestMethod]
         public void HoogsteBodIsEersteBedragNaEersteBod()
         {
-            var veiling = new Veiling();
+            //var veiling = new Veiling();
             veiling.DoeBod(2000m);
             Assert.AreEqual(2000m, veiling.HoogsteBod);
         }
@@ -24,7 +31,7 @@ namespace TDDTakenTest
         [TestMethod]
         public void HoogsteBodAlsMeerdereBiedingenZijnGedaanIsHoogsteBod()
         {
-            var veiling = new Veiling();
+            //var veiling = new Veiling();
             veiling.DoeBod(1000m);
             veiling.DoeBod(2000m);
             veiling.DoeBod(1500m);
