72a90adcab7524dc3f1f0f1b9ab08e063f434366
Dieter Van Staey <58091947+DieterVanStaey@users.noreply.github.com>
2021-10-14 13:15:43 +0200

    dependency injection

---
+++ TDDTakenTest/WinstServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDTaken;
+
+namespace TDDTakenTest
+{
+    [TestClass]
+    public class WinstServiceTest
+    {
+        private WinstService winstService;
+        private IKostDAO kostDAO;
+        private IOpbrengstDAO opbrengstDAO;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            kostDAO = new kostDAOStub();
+            opbrengstDAO = new opbrengstDAOStub();
+            winstService = new WinstService(opbrengstDAO, kostDAO);
+        }
+
+        [TestMethod]
+        public void Winst_OpbrengstMinKost_Is30()
+        {
+            Assert.AreEqual(30m, winstService.Winst);
+        }
+    }
+}
---
+++ TDDTakenTest/opbrengstDAOStub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDDTaken;
+
+namespace TDDTakenTest
+{
+    public class opbrengstDAOStub : IOpbrengstDAO
+    {
+        public decimal TotaleOpbrengst()
+        {
+            return 200m;
+        }
+    }
+}
